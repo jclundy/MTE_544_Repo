c6b5dcb06f45a78e2e8c0129161666904906d2a1
Jacob Rampertab <jacobrampertab@gmail.com>
2017-11-05 17:01:27 -0500

    Added setup script for netbook

---
+++ scripts/custom_netbook_setup.bash
@@ -0,0 +1,26 @@
+# Netbook Setup Script
+# Use this script on the netbook to setup the environment and
+# necessary scripts to run the robot
+
+# Setup ROS environment
+cd ~/catkin_ws
+source /opt/ros/kinect/setup.bash
+source ~/catkin_ws/devel/setup.bash
+export TURTLEBOT_3D_SENSOR=kinect
+export ROS_MASTER_URI=http://$(hostname -I):11311
+
+# Don't lock when lid closes
+LID_SWITCH="$(cat /etc/systemd/logind.conf | grep 'HandleLidSwitch=ignore')"
+if [ -n "$LID_SWITCH" ]; then
+    sudo echo "HandleLidSwitch=ignore" > /etc/systemd/logind.conf
+fi
+
+# Don't sleep
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# Spawn two terminals each running minimal build and kinect sensor code on netbook
+xterm -hold -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_bringup minimal.launch" &
+xterm -hold -e "cd ~/catkin_ws; source devel/setup.bash; roslaunch turtlebot_bringup 3dsensor.launch" &
+
+#Echo static IP for connection of other laptops
+echo "Static IP to connect to: $(hostname -I)"
